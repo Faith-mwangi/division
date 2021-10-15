78a7616e41ff54ae2f768e0972ba8f1dbf2a1667
Faith-mwangi <91890451+Faith-mwangi@users.noreply.github.com>
2021-10-15 10:38:30 +0300

    Add files via upload

---
+++ faith 2021/c programming.c
@@ -0,0 +1,13 @@
+ /* arithmetic operation division */
+#include <stdio.h>
+int main()
+{
+int a,b,divide;
+printf("enter value to a\n:");
+scanf("%d",&a);
+printf("enter value to b\n:");
+scanf("%d",&b);
+divide=a/b;
+printf("divide=%d",divide);
+return 0;
+}
